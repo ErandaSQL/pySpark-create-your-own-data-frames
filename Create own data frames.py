91e5688ec8137f4cb5d1d11e70ba8ba2c8fb16c8
ErandaSQL <46160087+ErandaSQL@users.noreply.github.com>
2019-04-26 09:43:00 +0300

    Update Create own data frames.py

--- Create own data frames.py
+++ Create own data frames.py
@@ -1,6 +1,6 @@
-# Databricks notebook source
+
 #suppose you want to create dataframs from scratch
-#First i'm creating a list to store list of values
+#First, i'm creating a list to store list of values
 #Lists and arrays are similar with few differences, therefore I will use lists
 
 #First set of values,[] marks the start and the end and () seperate value groups
@@ -21,62 +21,41 @@ useValues = [(1,'DS','Data Science'),(2,'ST','Statistics'),(3,'BD','Big Data')]
 useDF = spark.createDataFrame(useValues,['id','use','useDescr'])
 useDF.show()
 
-#you want to show language and useDescr
+#if you want to show language and useDescr
 #first, let's try data frame method to join two tables 
 #the syntax is <<new data frame>>=<<data frame>>.join(<<data frame,<<common field>>,<<join type>>)
 #based on the join type(especially the left join) positioning of data frames are important
 languageUseDF = languageDF.join(useDF, languageDF.use == useDF.use,'inner')
 languageUseDF.select('language','useDescr').show()
-#if you dont see results, you can scroll down in the results frame to see all results
+#In data bricks, if you dont see results, you can scroll down in the results frame to see all results
 
 #SQL method
-#spark.sql('select language,useDescr from languageDF LD inner join useDF UD on LD.use=UD.use').show()
+spark.sql('select language,useDescr from languageDF LD inner join useDF UD on LD.use=UD.use').show()
 # but your code doesn't work, you will get an error. why is that? 
 # the reson is, for spark SQL to access dataframes, dataframes should be in catalog first
-#print(spark.catalog.listTables())
-
-languageDF.createOrReplaceTempView('languageDF')
-
+# when you do this you can't see languageDF
 print(spark.catalog.listTables())
-#languageDF.createView('languageDF')
-#useDF.createOrReplaceTempView('useDF')
-#spark.sql('select language,useDescr from languageDF LD inner join useDF UD on LD.use=UD.use').show()
+#let's add data frame to catalog
+languageDF.createOrReplaceTempView('languageDF')
+#now you can see the table
+print(spark.catalog.listTables())
+#mind you, this is just a view of the existing table
+
+#do the same for useDF
+useDF.createOrReplaceTempView('useDF')
+
+#now SQL should work
+spark.sql('select language,useDescr from languageDF LD inner join useDF UD on LD.use=UD.use').show()
+
 
 #incase you want to delete any DF from catalog
 #spark.catalog.dropTempView('languageDF')
 #spark.catalog.dropTempView('useDF')
 
-
-# COMMAND ----------
-
-
-languageDF.createOrReplaceTempView('languageDF')
+#suppose you want to share the same data frame in multiple sessions\ multiple notebooks, then you have to create a global table or a view
+languageDF.createOrReplaceGlobalTempView('languageDF')
 useDF.createOrReplaceGlobalTempView('useDF')
 
-print(spark.catalog.listTables)
-#languageDF.createView('languageDF')
-#useDF.createOrReplaceTempView('useDF')
-#spark.sql('select language,useDescr from languageDF LD inner join useDF UD on LD.use=UD.use').show()
-
-#incase you want to delete any DF from catalog
-#spark.catalog.dropTempView('languageDF')
-#spark.catalog.dropTempView('useDF')
-
-# COMMAND ----------
-
-spark.session.stop()
-print(spark.catalog.listTables)
-
-
-# COMMAND ----------
-
-# Import SparkSession from pyspark.sql
-from pyspark.sql import SparkSession
-
-# Create my_spark
-my_spark = SparkSession.builder.getOrCreate()
-
-print(my_spark.catalog.listTables)
-
-my_spark.session.stop()
-print(my_spark.catalog.listTables)
+#to test this, open a new notebook from pySpark by going to databricks home https://community.cloud.databricks.com
+# then chose notebook and enter below code to refer the data frame 
+spark.sql('SELECT * FROM global_temp.languageDF').show()
